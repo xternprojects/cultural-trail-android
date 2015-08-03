5e7692c8a8eb7f980edb97c479d1a74ddad7efff
Kyle <pottsk@purdue.edu>
2015-08-03 13:56:33 -0400

    small

--- app/src/main/java/com/xtern/cultural_trail/cards/IssueListCardItemView.java
+++ app/src/main/java/com/xtern/cultural_trail/cards/IssueListCardItemView.java
@@ -42,12 +42,10 @@ public class IssueListCardItemView extends CardItemView<IssueListCard> {
         TextView description = (TextView)findViewById(R.id.issue_text);
         description.setText(card.getDescription());
 
+
         ImageView issuePicture = (ImageView)findViewById(R.id.issue_image);
-        Picasso
-                .with(getContext())
-                .load(card.getImageUrl())
-                .into(issuePicture);
-        //Ion.with(issuePicture).load(card.getImageUrl());
+
+        Ion.with(issuePicture).load(card.getImageUrl());
 
         TextView location =(TextView)findViewById(R.id.location_text_view);
         location.setText(card.getLocation());
--- app/src/main/java/com/xtern/cultural_trail/fragments/IssuesListFragment.java
+++ app/src/main/java/com/xtern/cultural_trail/fragments/IssuesListFragment.java
@@ -53,8 +53,6 @@ public class IssuesListFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_issues_list, container, false);
         materialListView = (MaterialListView)v.findViewById(R.id.material_listview);
-        progressWheel = (ProgressWheel)v.findViewById(R.id.progress_wheel);
-        progressWheel.spin();
         materialListView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
 
             @Override
@@ -119,8 +117,6 @@ public class IssuesListFragment extends Fragment {
                     //materialListView.add(issueListCard);
                 }
                 materialListView.addAll(cards);
-                materialListView.getAdapter().notifyDataSetChanged();
-                progressWheel.setVisibility(View.INVISIBLE);
             }
 
             @Override
